f5e93f7e4b9e5d3e2c2d841daab8b81ddea0e32e
Marwan Zogheib <mzogheib@gmail.com>
2021-10-23 17:45:36 +1100

    More examples

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,11 @@ function App() {
           Today's date is <FormattedDate value={Date.now()} />
         </div>
         <div>
-          This is a formatted number <FormattedNumber value={2000.45} />
+          This is a formatted number <FormattedNumber value={10000.45} />
+        </div>
+        <div>
+          This is a formatted currency{' '}
+          <FormattedNumber value={2000.32} style={`currency`} currency="USD" />
         </div>
         <FormattedPlural value={5} one="5 dog" other="5 dogs" />
       </div>
